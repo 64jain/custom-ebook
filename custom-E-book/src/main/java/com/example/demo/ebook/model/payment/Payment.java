f0fe3bcac32859a8b0887637bd1f023b635a5bd7
sam1096 <Samridhi.Kundaliya@iiitb.org>
2019-04-14 10:33:04 +0530

    made pojo elements

--- custom-E-book/src/main/java/com/example/demo/ebook/model/payment/Payment.java
+++ custom-E-book/src/main/java/com/example/demo/ebook/model/payment/Payment.java
@@ -1,5 +1,51 @@
 package com.example.demo.ebook.model.payment;
-
+import javax.persistence.*;
+@Entity
 public class Payment {
-
+	@Id
+	@GeneratedValue
+	private int id;
+	private int buyerid;
+	private int ebookid;
+	private boolean copy_type;
+	private double price;
+	private String payment_method;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getBuyerid() {
+		return buyerid;
+	}
+	public void setBuyerid(int buyerid) {
+		this.buyerid = buyerid;
+	}
+	public int getEbookid() {
+		return ebookid;
+	}
+	public void setEbookid(int ebookid) {
+		this.ebookid = ebookid;
+	}
+	public boolean isCopy_type() {
+		return copy_type;
+	}
+	public void setCopy_type(boolean copy_type) {
+		this.copy_type = copy_type;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getPayment_method() {
+		return payment_method;
+	}
+	public void setPayment_method(String payment_method) {
+		this.payment_method = payment_method;
+	}
+	
 }
